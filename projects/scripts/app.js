9b2d36cb78ed6cd6042bc34e73870392876058d2
Katrina Engelsted <katrina.engelsted@hotosm.org>
2014-09-25 15:59:44 -0600

    Update app.js

--- projects/scripts/app.js
+++ projects/scripts/app.js
@@ -2,111 +2,117 @@ var App, NPMap;
 
 App = {
   types: [
-    'travelTime',
-    'WCMComposite',
-    'WCMNaturalQuality',
-    'WCMSolitudeQuality',
-    'WCMUndevelopedQuality',
-    'WCMUntrammeledQuality',
-    'ViewshedInsideWildernes'
-  ],
-  layerVisible: function() {
-    var active = [],
-      i;
-
-    for (i = 0; i < this.types.length; i++) {
-      var type = this.types[i];
-
-      if (document.getElementById(type).checked) {
-        active.push(type);
-      }
-    }
-
-    NPMap.config.overlays[0].L.eachLayer(function(layer) {
-      var properties = layer.feature.properties,
-        visible = false;
-
-      for (i = 0; i < active.length; i++) {
-        if (properties[active[i]]) {
-          visible = true;
-          break;
-        }
-      }
-
-      if (visible) {
-        NPMap.config.L.addLayer(layer);
-      } else {
-        NPMap.config.L.removeLayer(layer);
-      }
-    });
-  }
+    "SEKI_SOCIO_TravelTime",
+    "SEKI_SOCIO_WCMComposite",
+    "SEKI_SOCIO_WCMNaturalQuality",
+    "SEKI_SOCIO_WCMSolitudeQuality",
+    "SEKI_SOCIO_WCMUndevelopedQuality",
+    "SEKI_SOCIO_WCMUntrammeledQuality",
+    "SEKI_SOCIO_ViewshedInsideWildernes"
+  ]
 };
 
-NPMap = {
+var NPMap = {
   baseLayers: [
     "nps-parkTiles"
   ],
-  center: {
-    lat: 36.70806354647625,
-    lng: -118.46145629882811
+  "center": {
+    lat: "36.70806354647625",
+    lng: "-118.46145629882811"
   },
   description: "Wilderness Character Rasters",
   div: "map",
   editControl: true,
   homeControl: true,
+  hooks: {
+    preinit: function(callback) {
+      L.npmap.util._.reqwest({
+        success: function(response) {
+
+            layerVisible = function() {
+              var active = [],
+              overlay = NPMap.config.overlays[0],
+              i;
+
+              for (i = 0; i < App.types.length; i++) {
+                var type = App.types[i];
+
+              if (document.getElementById(type).checked) {
+                active.push(type);
+              } else {
+                for (i = 0; i < response.layers.length; i++) {
+                  response.layers[i].defaultVisibility = false;
+                }
+            }
+          }
+
+            for (i = 0; i < response.layers.length; i++) {
+                if (response.layers[i].name.indexOf(active) > -1){
+                  response.layers[i].defaultVisibility = true;
+                }
+
+              if (response.layers[i].defaultVisibility === true) {
+                NPMap.config.L.addLayer(overlay.L);
+              } else {
+                NPMap.config.L.removeLayer(overlay.L);
+              }
+           }
+          }
+
+          var html = '<table>',
+          i;
+
+          for (i = 0; i < response.layers.length; i++) {
+            var layer = response.layers[i],
+            name = layer.name;
+
+            if (name === 'SEKI_SOCIO_WCMUntrammeledQuality') {
+              html += '' +
+              '<tr>' +
+              '<td style="vertical-align:bottom !important;padding-right: 2px;"><input id="' + name + '" name="checkLayers" onchange="layerVisible();return false;" type="checkbox" checked></td>' +
+              '<td><label for="'+ name + '">'+ name + '</label></td>' +
+            '</tr>'
+            } else {
+              html += '' +
+              '<tr>' +
+              '<td style="vertical-align:bottom !important;padding-right: 2px;"><input id="' + name + '" name="checkLayers" onchange="layerVisible();return false;" type="checkbox"></td>' +
+              '<td><label for="'+ name + '">'+ name + '</label></td>' +
+            '</tr>'
+            }
+          };
+
+          NPMap.config.legendControl = {
+            html: html + '</table>',
+            position: 'bottomleft'
+          };
+        callback();
+      },
+      type: 'jsonp',
+      url: 'http://inpsekigistest/arcgis/rest/services/SEKI/SEKI_SOCIO/MapServer/?f=json'
+    })
+   }
+  },
   locateControl: true,
   mapId: "c7169847-be64-461f-9623-ca7d512c3d57",
   measureControl: true,
   name: "SEKI Wilderness Character Test",
-  overlays: [
-    {
-      layers: "0,1,2,3,4,5,6",
-      opacity: 1,
-      tiled: false,
-      type: "arcgisserver",
-      url: "http://inpsekigistest/arcgis/rest/services/SEKI/SEKI_SOCIO/MapServer",
-      attribution: "{{Value}}",
-      description: "SEKI SOCIO Rasters",
-      name: "SEKI SOCIO Rasters",
-      popup: {
-        description: "{{Pixel Value}}",
-        title: "{{[Pixel Value]}}"
-      }
+  overlays: [{
+    layers: "0,1,2,3,4,5,6",
+    opacity: 1,
+    tiled: false,
+    type: "arcgisserver",
+    url: "http://inpsekigistest/arcgis/rest/services/SEKI/SEKI_SOCIO/MapServer",
+    attribution: "{{Value}}",
+    description: "SEKI SOCIO Rasters",
+    name: "SEKI SOCIO Rasters",
+    popup: {
+      description: "{{Pixel Value}}",
+      title: "{{[Pixel Value]}}"
     }
-  ],
-  legendControl: {
-    html: '<table>' +
-      '<tr>' +
-        '<td style="vertical-align:bottom !important;padding-right: 2px;"><input id="travelTime" name="checkLayers" onchange="App.layerVisible();return false;" type="checkbox" checked></td>' +
-        '<td><label for="travelTime">Travel Time</label></td>' +
-      '</tr>' +
-      '<tr>' +
-        '<td style="vertical-align:bottom !important; padding-right: 2px;"><input id="WCMComposite" name="checkLayers" onchange="App.layerVisible();return false;" type="checkbox" checked></td>'+
-        '<td><label for="WCMComposite">WCM Composite</label></td>' +
-      '</tr>' +
-      '<tr>' +
-        '<td style="vertical-align:bottom !important;padding-right: 2px;"><input id="WCMNaturalQuality" name="checkLayers" onchange="App.layerVisible();return false;" type="checkbox" checked ></td>'+
-        '<td><label for="WCMNaturalQuality">WCM Natural Quality</label></td>' +
-      '</tr>' +
-            '<tr>' +
-        '<td style="vertical-align:bottom !important;padding-right: 2px;"><input id="WCMSolitudeQuality" name="checkLayers" onchange="App.layerVisible();return false;" type="checkbox" checked></td>' +
-        '<td><label for="WCMSolitudeQuality">WCM Solitude Quality</label></td>' +
-      '</tr>' +
-      '<tr>' +
-        '<td style="vertical-align:bottom !important; padding-right: 2px;"><input id="WCMUndevelopedQuality" name="checkLayers" onchange="App.layerVisible();return false;" type="checkbox" checked></td>'+
-        '<td><label for="WCMUndevelopedQuality">WCM Undeveloped Quality</label></td>' +
-      '</tr>' +
-      '<tr>' +
-        '<td style="vertical-align:bottom !important;padding-right: 2px;"><input id="WCMUntrammeledQuality" name="checkLayers" onchange="App.layerVisible();return false;" type="checkbox" checked ></td>'+
-        '<td><label for="WCMUntrammeledQuality">WCM Untrammeled Quality</label></td>' +
-      '</tr>' +
-      '<tr>' +
-        '<td style="vertical-align:bottom !important;padding-right: 2px;"><input id="ViewshedInsideWilderness" name="checkLayers" onchange="App.layerVisible();return false;" type="checkbox" checked ></td>'+
-        '<td><label for="ViewshedInsideWildernes">Viewshed Inside Wilderness</label></td>' +
-      '</tr>' +
-    '</table>',
-    position: 'bottomleft'
-  }
+  }],
+  printControl: true,
+  smallzoomControl: true,
+  zoom: 9
 };
 
 var s = document.createElement('script');
