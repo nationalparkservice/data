aca14eb25de758131c8663d98de55d9d23a3c525
jcant624 <jcant624@gmail.com>
2016-05-06 15:09:14 -0600

    Added files via upload

--- projects/water_resources_division/js/map_viis.js
+++ projects/water_resources_division/js/map_viis.js
@@ -53,7 +53,7 @@ var NPMap = {
         var name = App.types[i].toLowerCase().replace(/\s/g, ''),
           formalName = App.types[i];
 
-		NPMap.config.overlays.push({type: 'cartodb', user: 'npswrd', table: name, name: formalName, popup: { description: '{{{popupinfo}}}', title: '{{name}}' }});
+		NPMap.config.overlays.push({type: 'cartodb', user: 'npswrd', table: name, name: formalName,, sql: 'SELECT * FROM ' + name + ' ORDER BY cartodb_id', popup: { description: '{{{popupinfo}}}', title: '{{name}}' }});
 
         var layer = NPMap.config.overlays[i];
 
